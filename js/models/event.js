677ee5b21d6d9ce4810110113782bac94084a2ce
David Currie <curriedav@gmail.com>
2014-01-11 23:22:34 -0800

    add group model

--- js/models/event.js
+++ js/models/event.js
@@ -1,1 +1,17 @@
+var EventModel = Backbone.Model.extend({
+		eventObj: {},
+		groupIds: [6693792,4808882,8407282],
+
+
+	meetUpEventRequest: function (groups) {
+		var apiKey = "936f3b161c2450506d7b23683319",
+		 	url = "https://api.meetup.com/2/events?&sign=true&group_id=" + groupIds + "&key=" + apiKey + "&page=20";
+
+		$.getJSON(url + "&callback=?", null, function(eventData) {
+		 	eventObj = eventData;
+		 	console.log(eventObj);
+		});
+	};
+});
+
 module.exports = EventModel;
--- js/views/group.js
+++ js/views/group.js
@@ -1,1 +1,16 @@
+var GroupView = Backbone.View.extend({
+  el: '#groups',
+
+  template: require('../templates/groups.hbs'),
+
+  initialize: function () {
+    this.listenTo(this.model, 'change', this.render);
+  },
+
+  render: function () {
+    this.$el.html(this.template(this.model.get('groupObj') || {data: []} ));
+    return this;
+  }
+});
+
 module.exports = GroupView;
