b3174f5239db8b500283ece36c53ef317ba09d19
David Currie <curriedav@gmail.com>
2014-01-13 20:46:44 -0800

    adds a view and styling

--- js/views/event.js
+++ js/views/event.js
@@ -1,16 +1,17 @@
 var EventView = Backbone.View.extend({
-  el: '#events',
+  	el: '#events',
 
-  template: require('../../templates/events.hbs'),
+  	template: require('../../templates/events.hbs'),
 
-  initialize: function () {
-    this.render;
-  },
-
-  render: function () {
-    this.$el.html(this.template(this.model.get('eventObj') || {data: []} ));
-    return this;
-  }
+  	initialize: function () {
+    	this.listenTo(this.model, 'change', this.render);
+    	this.render;
+  	},
+	
+	render: function () {
+	   	this.$el.html(this.template(this.model.get('results') || { data: [] }));
+	    return this;
+	}
 });
 
 module.exports = EventView;
