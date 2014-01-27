a65e5d029eb24fe77083662785bc5e3a6dfd7adf
David Currie <curriedav@gmail.com>
2014-01-27 15:24:19 -0800

    style changes

--- js/views/event.js
+++ js/views/event.js
@@ -9,9 +9,35 @@ var EventView = Backbone.View.extend({
   	},
 	
 	render: function () {
-	   	this.$el.html(this.template(this.model.get('results') || { data: [] }));
+	   	var self = this,
+      data = this.model.get('results') || { data: [] };
+      
+      data.forEach(function (mut) {
+        mut.formattedTime = self.formatMeetupTime(mut.time);
+      });
+
+      console.log(data);
+      this.$el.html(this.template(data));
 	    return this;
-	}
+	},
+
+  formatMeetupTime: function (time) {
+    var obj = "",
+    week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
+    month = ["Jan.", "Feb.", "Mar.", "Apr.", "May", "June", "July", "Aug.", "Sept.", "Oct.", "Nov.", "Dec."],
+    timeStamp = new Date(time),
+    hours = timeStamp.getHours(),
+    mins = ":0" + timeStamp.getMinutes(),
+    time;
+
+    if (hours > 12) {
+      time = hours - 12 + mins + " PM";
+    } else {
+      time = hours + mins + " AM";
+    }
+
+    return obj = week[timeStamp.getDay()] + ", " + month[timeStamp.getMonth()] + " " + timeStamp.getDate() + " at " + time;
+  }
 });
 
 module.exports = EventView;
